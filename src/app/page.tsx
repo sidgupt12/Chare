d24a91d5d665aa755fb8f382dddb9e06201781ec
sidgupt12 <sidgupt12@gmail.com>
2025-03-03 11:58:33 +0530

    created webhook for convex and clerk user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <div> 
+      <SignInButton />
+      <SignOutButton />
     </div>
   );
 }
